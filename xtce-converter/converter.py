917a61bc4c2877c79c6d46ca658f4b6b2759eee4
Samuel Baker <sbaker.personal@gmail.com>
2025-10-31 16:26:53 -0400

    feat: parameterize csv

--- xtce-converter/converter.py
+++ xtce-converter/converter.py
@@ -1,4 +1,5 @@
 import requests
+import csv
 
 sheet_id = "1Ukaums3NfbJdVOQL7E1QMyPNoQ7gD5Zxciiz4ucRUrk"
 gid = "2140536820"
@@ -9,3 +10,19 @@ response = requests.get(url)
 if response.status_code == 200:
     with open("data.csv", "wb") as f:
         f.write(response.content)
+
+with open("data.csv", newline="") as f:
+    reader = csv.reader(f)
+    headers = next(reader)[1:]
+    rows = [row for row in reader]
+    parameters = []
+
+    for col in range(1, len(headers)+1):
+        column = []
+        for row in rows:
+            if row[col] != "":
+                column.append(row[col])
+        parameters.append(column)
+
+    parameterized_data = dict(zip(headers, parameters))
+    print(parameterized_data)
