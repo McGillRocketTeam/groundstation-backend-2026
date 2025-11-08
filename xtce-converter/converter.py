d79b86ddb7826935071275b74dd8f7242c517d65
Samuel Baker <sbaker.personal@gmail.com>
2025-11-08 18:27:31 -0500

    Refactor converter.py

--- xtce-converter/converter.py
+++ xtce-converter/converter.py
@@ -1,5 +1,4 @@
 from unittest import case
-
 import yamcs.pymdb as Y
 import requests
 import csv
@@ -7,7 +6,6 @@ import csv
 sheet_id = "1Ukaums3NfbJdVOQL7E1QMyPNoQ7gD5Zxciiz4ucRUrk"
 gid = "2042306306"
 url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv&gid={gid}"
-
 response = requests.get(url)
 
 if response.status_code == 200:
@@ -22,9 +20,14 @@ with open("data.csv", newline="") as f:
     headers = next(reader)
     data = [dict(zip(headers, row)) for row in reader]
 
+def parametrize(param, bit_pos, number):
+    entry = Y.ParameterEntry(param, bitpos=bit_pos)
+    param_array.append(entry)
+    bit_pos += number
+    return bit_pos
+
 param_array = []
 bit_pos = 0
-
 for row in data:
     GUI_type = row["GUI Type"]
     kwargs = dict(
@@ -34,59 +37,50 @@ for row in data:
         long_description=row["Description (optional)"],
     )
 
-
     match GUI_type:
         case "AbsoluteTime":
             param = Y.AbsoluteTimeParameter(**kwargs, reference=Y.Epoch(""))
-            entry = Y.ParameterEntry(param)
-            param_array.append(entry)
+            bit_pos = parametrize(param, bit_pos, 0)
         case "Binary":
             param = Y.BinaryParameter(**kwargs)
-            entry = Y.ParameterEntry(param)
-            param_array.append(entry)
+            bit_pos = parametrize(param, bit_pos, 0)
         case "Boolean":
             param = Y.BooleanParameter(**kwargs, encoding=Y.IntegerEncoding(bits=1))
-            entry = Y.ParameterEntry(param, bitpos=bit_pos)
-            bit_pos += 1
-            param_array.append(entry)
+            bit_pos = parametrize(param, bit_pos, 1)
         case "Enumerated":
             choices = []
             meta = row["Metadata/Notes"]
             enc = row["Encoding"]
             number = int("".join(filter(str.isdigit, enc)))
-
             if meta:
                 for token in meta.splitlines():
                     if "=" in token:
                         k, v = token.split("=", 1)
                         choices.append((int(k.strip()), v.strip()))
             param = Y.EnumeratedParameter(**kwargs, choices=choices, encoding=Y.IntegerEncoding(bits=number))
-            entry = Y.ParameterEntry(param, bitpos=bit_pos)
-            bit_pos += number
-            param_array.append(entry)
+            bit_pos = parametrize(param, bit_pos, number)
         case "Float":
+            # cal = row["Calibration Function f(x)"]
+            # if cal:
+            #     calibrator = None
+            # else:
+            #     calibrator = None
             enc = row["Encoding"]
             number = int("".join(filter(str.isdigit, enc)))
             if "uint" in enc:
-                param = Y.FloatParameter(**kwargs, encoding=Y.IntegerEncoding(bits=number))
+                param = Y.FloatParameter(**kwargs, encoding=Y.IntegerEncoding(bits=number)) #calibrator = calibrator
             elif "float" in enc:
-                param = Y.FloatParameter(**kwargs, encoding=Y.FloatEncoding(bits=number))
-            entry = Y.ParameterEntry(param, bitpos=bit_pos)
-            bit_pos += number
-            param_array.append(entry)
+                param = Y.FloatParameter(**kwargs, encoding=Y.FloatEncoding(bits=number)) #calibrator = calibrator
+            bit_pos = parametrize(param, bit_pos, number)
         case "Integer":
             enc = row["Encoding"]
             number = int("".join(filter(str.isdigit, enc)))
             scheme =  Y.IntegerEncodingScheme.UNSIGNED if "u" in enc else Y.IntegerEncodingScheme.ONES_COMPLEMENT
             param = Y.FloatParameter(**kwargs, encoding=Y.IntegerEncoding(bits=number, scheme=scheme))
-
-            entry = Y.ParameterEntry(param, bitpos=bit_pos)
-            bit_pos += number
-            param_array.append(entry)
+            bit_pos = parametrize(param, bit_pos, number)
         case "String":
             param = Y.StringParameter(**kwargs)
-            entry = Y.ParameterEntry(param)
-            param_array.append(entry)
+            bit_pos = parametrize(param, bit_pos, 0)
 
     # cal = row.get("Calibration Function f(x)", "").strip()
     # if cal:
